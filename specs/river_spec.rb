bb55b777b4f1f8981b4e693a2d9a307735337a62
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-06-23 16:33:06 +0100

    river starts with zero fish test passes

--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -11,8 +11,13 @@ class RiverTest < MiniTest::Test
         @river = River.new("Dunajec")
     end
 
-    def test_river_has_name
+    def test_has_name
         assert_equal("Dunajec", @river.name)
     end
+
+    def test_starts_with_zero_fish
+        assert_equal(0, @river.fish_count)
+    end
+
     
 end
